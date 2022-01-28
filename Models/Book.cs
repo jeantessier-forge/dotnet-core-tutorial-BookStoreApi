681f0a73b43e5ce1f07dca526eeaa3763f564b0e
Jean Tessier <jean.tessier@forgeglobal.com>
2022-01-27 19:11:15 -0800

    Remove null value warning

--- Models/Book.cs
+++ Models/Book.cs
@@ -8,7 +8,7 @@ namespace BookStoreApi.Models
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
-        public string? Id { get; set; }
+        public string Id { get; set; } = string.Empty;
 
         [BsonElement("Name")]
         [JsonPropertyName("Name")]
